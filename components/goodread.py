d7320dc871225798c0a185676d4a6e8682608c41
CxrlosKenobi <kenobi@ieee.org>
2022-01-15 11:57:42 -0300

    Co-authored-by: Rodrigo Flores <Rodrigo-Flores@users.noreply.github.com>

--- components/goodread.py
+++ components/goodread.py
@@ -6,6 +6,7 @@ from datetime import datetime
 import pytz
 import json
 from random import randint
+from random import random
 
 tz = pytz.timezone('America/Santiago')
 dt = datetime.now(tz)
@@ -16,25 +17,30 @@ with open('data.json', 'r') as file:
 
 def push(highlight, verified):
     if verified is True:
+        authors = {}
         with open('data/records.json', 'r') as file:
             hist = json.load(file)
-            # Check efficiency for this sort later
-            hist['0'] = hist['1']
-            hist['1'] = hist['2']
-            hist['2'] = hist['3']
-            hist['3'] = hist['4']
-            hist['4'] = hist['5']
-            hist['5'] = hist['6']
-            hist['6'] = hist['7']
-            hist['7'] = hist['8']
-            hist['8'] = hist['9']
-            hist['9'] = hist['10']
-            hist['10'] = hist['11']
-            hist['11'] = hist['12']
-            hist['12'] = hist['13']
-            hist['13'] = hist['14']
-            hist['14'] = hist['15']
-            hist['15'] = highlight
+
+            # for i in range(len(hist)):
+            #     if hist.author in authors:
+            #         authors[hist.author][0] += 1
+            #     else:
+            #         authors[hist.author] = [1, hist.highlight]
+
+            # authors.sort(reverse=True)
+
+            # for i in authors:
+            #     n = random()
+            #     if n < authors[i]:
+            #         clipping = authors[i][1]
+
+            quotaLimit = 15  # Calcular porcentaje
+            for i in range(quotaLimit + 1):
+                if i < quotaLimit:
+                    hist[i] = hist[i+1]
+                else:
+                    hist[i] = highlight
+
             with open('data/records.json', 'w') as file:
                 json.dump(hist, file, indent=4)
     else:
@@ -42,11 +48,13 @@ def push(highlight, verified):
             hist = json.load(file)
             return hist
 
+
 def btnMode(update, context):
     update.message.reply_text(
         text='Type something...',
         reply_markup=InlineKeyboardMarkup([
-            [InlineKeyboardButton(text='Morning Mode', callback_data='morning')],
+            [InlineKeyboardButton(text='Morning Mode',
+                                  callback_data='morning')],
             [InlineKeyboardButton(text='Noon Mode', callback_data='noon')],
             [InlineKeyboardButton(text='Night Mode', callback_data='night')]
         ])
@@ -60,8 +68,9 @@ def morningMode():
     name = 'Have a good read at morning!'
     days = (0, 1, 2, 3, 4, 5, 6)
     time = dt.replace(hour=h, minute=m, second=0, microsecond=0)
-    
+
     return time, days, name
+
 
 def noonMode():
     h = randint(13, 18)
@@ -73,6 +82,7 @@ def noonMode():
 
     return time, days, name
 
+
 def nightMode():
     h = randint(20, 22)
     m = randint(0, 60)
@@ -82,4 +92,3 @@ def nightMode():
     time = dt.replace(hour=h, minute=m, second=0, microsecond=0)
 
     return time, days, name
-
