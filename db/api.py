4b672e6bf0c162003cd692694868f46d246246b3
CxrlosKenobi <kenobi@ieee.org>
2022-02-13 01:48:04 -0300

    Update API with DB callbacks

--- db/api.py
+++ db/api.py
@@ -1,7 +1,31 @@
-import sqlite3
-import pandas as pd
-import datetime as dt
 import pathlib
+import sqlite3
+#
+from random import randint
+#
+from components.goodread import push
 
 DB_FILE = pathlib.Path(__file__).resolve().parent.joinpath('clips.db').resolve()
 
+def getQuotesQuantity():
+  conn = sqlite3.connect(DB_FILE)
+  c = conn.cursor()
+  c.execute('SELECT COUNT(*) FROM clips')
+  return c.fetchone()[0]
+
+
+def getQuote():
+  clippingsLength = getQuotesQuantity()
+  seed = randint(0, clippingsLength - 1)
+  push(id, False)
+
+  conn = sqlite3.connect(DB_FILE)
+  c = conn.cursor()
+  c.execute('SELECT highlight, author, book FROM clips WHERE id = ?', (seed,))
+  highlight, author, book = c.fetchone()
+
+  return highlight, author, book
+
+
+q = getQuote()
+print(q)
