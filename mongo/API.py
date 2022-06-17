09f6cd1c8e3dca397512bb4ed3cc6eff57aed18a
CxrlosKenobi <kenobi@ieee.org>
2022-06-17 16:20:37 -0400

    Init base to mongodb integration

---
+++ mongo/API.py
@@ -0,0 +1,7 @@
+# from  pymongo import MongoClient
+
+# client = MongoClient("mongodb+srv://kenobi:6235539a@sandbox.hdyam.mongodb.net/?retryWrites=true&w=majority")
+
+# db = client.test
+
+# Prinrt 
