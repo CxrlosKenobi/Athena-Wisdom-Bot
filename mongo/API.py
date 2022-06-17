bd270be29750f769c6246a226073d8780c4fd164
CxrlosKenobi <kenobi@ieee.org>
2022-06-17 16:40:46 -0400

    Deleted (now) unnecessary sql files

--- mongo/API.py
+++ mongo/API.py
@@ -1,7 +1,5 @@
-# from  pymongo import MongoClient
+from  pymongo import MongoClient
 
-# client = MongoClient("mongodb+srv://kenobi:6235539a@sandbox.hdyam.mongodb.net/?retryWrites=true&w=majority")
-
-# db = client.test
-
-# Prinrt 
+client = MongoClient("mongodb+srv://kenobi:6235539a@sandbox.hdyam.mongodb.net/admin")
+db = client.diana_wisdom
+collection = db.quotes
