97b0ba4f5b2a73ee4810c28d738f2a09c44da000
CxrlosKenobi <kenobi@ieee.org>
2022-06-23 00:36:37 -0400

    Update for semantic functions

--- components/fetch.py
+++ components/fetch.py
@@ -1,11 +1,11 @@
 import json
 
-def fetch_token():
+def fetch_token() -> str:
   with open("config.json", "r") as file:
     get = json.load(file)
   return get["token"]
 
-def fetch_db_url():
+def fetch_db_url() -> str:
   with open("config.json", "r") as file:
     get = json.load(file)
   return get["db_url"]
