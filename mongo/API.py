fbd0ec960ab45c97b894f85fe9b8c7a33df4dfe3
CxrlosKenobi <kenobi@ieee.org>
2022-06-23 00:35:55 -0400

    Update API requests

--- mongo/API.py
+++ mongo/API.py
@@ -3,19 +3,23 @@ from random import randint
 #
 from components.fetch import fetch_db_url
 
-def connect():
-  client = MongoClient(fetch_db_url)
+def connect() -> MongoClient:
+  client = MongoClient(fetch_db_url())
   db = client.diana_wisdom
   collection = db.quotes
 
   return collection
 
-def get_random_quote():
+def get_random_quote() -> dict:
   collection = connect()
   _max = collection.count_documents({})
   _random = randint(1, _max)
-  quote = collection.find_one({'id': _random})
+  quote = collection.find_one({ 'id': _random })
 
   return quote
 
+def get_docs_count() -> int:
+  collection = connect()
+  _max = collection.count_documents({})
 
+  return _max
